a98d4358a7d5f38479dacb38119364198188c3ca
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-02-15 20:25:28 +0000

    class updated

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,63 +1,71 @@
 #!/usr/bin/env python3
 """
-Module to handle session authentication route
+Definition of class SessionAuth
 """
-from werkzeug import exceptions
-from api.v1.views import app_views
+import base64
+from uuid import uuid4
+from typing import TypeVar
+
+from .auth import Auth
 from models.user import User
-from flask import jsonify, request
-from os import abort, environ, getenv
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def session_login() -> str:
+class SessionAuth(Auth):
+    """ Implement Session Authorization protocol methods
     """
-    POST /api/v1/auth/session/login
-    Return:
-        user instance
-        else
-            400 if missing
-    """
-    user_email = request.form.get('email', None)
-    user_password = request.form.get('password', None)
+    user_id_by_session_id = {}
 
-    if user_email is None or user_email == "":
-        return jsonify({"error": "email missing"}), 400
-    if user_password is None or user_password == "":
-        return jsonify({"error": "password missing"}), 400
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Creates a Session ID for a user with id user_id
+        Args:
+            user_id (str): user's user id
+        Return:
+            None is user_id is None or not a string
+            Session ID in string format
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        id = uuid4()
+        self.user_id_by_session_id[str(id)] = user_id
+        return str(id)
 
-    is_valid_user = User.search({'email': user_email})
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Returns a user ID based on a session ID
+        Args:
+            session_id (str): session ID
+        Return:
+            user id or None if session_id is None or not a string
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
 
-    if not is_valid_user:
-        return jsonify({"error": "no user found for this email"}), 404
+    def current_user(self, request=None):
+        """
+        Return a user instance based on a cookie value
+        Args:
+            request : request object containing cookie
+        Return:
+            User instance
+        """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+        user = User.get(user_id)
+        return user
 
-    is_valid_user = is_valid_user[0]
-
-    if not is_valid_user.is_valid_password(user_password):
-        return jsonify({"error": "wrong password"}), 401
-
-    from api.v1.app import auth
-    session_id = auth.create_session(is_valid_user.id)
-    cookie_response = getenv('SESSION_NAME')
-    user_dict = jsonify(is_valid_user.to_json())
-
-    user_dict.set_cookie(cookie_response, session_id)
-    return (user_dict)
-
-
-@app_views.route(
-    '/auth_session/logout',
-    methods=['DELETE'],
-    strict_slashes=False)
-def session_logout() -> str:
-    """
-    DELETE /api/v1/auth/session/logout
-    Returns:
-        deleted json (if correctly done)
-        else
-            404 if fails
-    """
-    from api.v1.app import auth
-    if auth.destroy_session(request):
-        return jsonify({}), 200
-    abort(404)
+    def destroy_session(self, request=None):
+        """
+        Deletes a user session
+        """
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
