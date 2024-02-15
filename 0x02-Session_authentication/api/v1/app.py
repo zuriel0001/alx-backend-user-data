fb0620a39e9a9888f45d43df6d43ecc408d4015b
zuriel0001 <dogbeykwame.kd@gmail.com>
2024-02-15 21:26:37 +0000

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -70,8 +70,7 @@ def before_request() -> str:
         '/api/v1/auth_session/login/']
     if auth:
         if auth.require_auth(request.path, request_path_list):
-            if auth.authorization_header(request) is None and
-            auth.session_cookie(request) is None:
+            if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
                 abort(403)
